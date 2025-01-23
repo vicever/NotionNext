723bfd5f9816716045d20b6e7440477a5653544e
vicever <vicever@yeah.net>
2025-01-23 13:49:32 +0800

    change web logo into transfered

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '野火燎原成新势，一路行来终有痕', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zhanggs.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'BigData , IT , Blog , AI , 博客', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/logo-new-4.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/web-logo.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
